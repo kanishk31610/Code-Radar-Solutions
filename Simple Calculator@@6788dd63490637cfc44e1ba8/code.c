0769b15060362707dd6e03ebbb619191955c09f6
kanishk31610 <kanishk1128.becse24@chitkara.edu.in>
2025-02-05 10:21:28 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,23 +1,54 @@
+// #include <stdio.h>
+
+// int main(){
+//     int a,b;
+//     char c;
+//     scanf("%d %d",&a,&b);
+//     scanf(" %c", %c);
+//     switch(c){
+//         case '+':
+//         printf("%d",a+b);
+//         break;
+//         case '-':
+//         printf("%d",a-b);
+//         break;
+//         case '*':
+//         printf("%d",a*b);
+//         break;
+//         case '/':
+//         printf("%d",a/b);
+//         break;
+//     }
+//     return 0;
+// }
+
+
+
 #include <stdio.h>
 
-int main(){
-    int a,b;
-    char c;
-    scanf("%d %d",&a,&b);
-    scanf(" %c", %c);
-    switch(c){
-        case '+':
-        printf("%d",a+b);
-        break;
-        case '-':
-        printf("%d",a-b);
-        break;
-        case '*':
-        printf("%d",a*b);
-        break;
-        case '/':
-        printf("%d",a/b);
-        break;
+int main() {
+    int a, b;
+    char op;
+
+    // Reading two integers and an operator
+    scanf("%d %d %c", &a, &b, &op);
+
+    // Checking operator and performing the corresponding operation
+    if (op == '+') {
+        printf("%d", a + b);
+    } else if (op == '-') {
+        printf("%d", a - b);
+    } else if (op == '*') {
+        printf("%d", a * b);
+    } else if (op == '/') {
+        if (b == 0) {
+            printf("Error: Division by zero");
+        } else {
+            printf("%d", a / b);
+        }
+    } else {
+        printf("Error: Invalid operator");
     }
+
     return 0;
 }
