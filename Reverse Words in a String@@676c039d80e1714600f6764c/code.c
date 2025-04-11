6612e7e65202fa07cb32a49fefebca76930f4281
kanishk31610 <kanishkarora2410@gmail.com>
2025-04-11 16:42:14 +0530

    Solution to Reverse Words in a String

--- Reverse Words in a String@@676c039d80e1714600f6764c/code.c
+++ Reverse Words in a String@@676c039d80e1714600f6764c/code.c
@@ -4,7 +4,7 @@
 int main(){
     char str[100];
     int length;
-    if(fgets(str,sizeof(str),stdin)!=null){
+    if(fgets(str,sizeof(str),stdin)!=NULL){
         str[strcspn(str,"\n")]='\0';
         length=strlen(str);
         for(int i =length-1;i>=0;i--){
